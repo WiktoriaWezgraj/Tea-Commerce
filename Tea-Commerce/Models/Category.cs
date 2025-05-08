651401968e713d070e9a29eebf8902d7fe199af2
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2025-05-08 22:33:57 +0200

    Add Product Controller + Basic Models

---
+++ Tea-Commerce/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Tea_Commerce.Models
+{
+    public class Category : BaseModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
